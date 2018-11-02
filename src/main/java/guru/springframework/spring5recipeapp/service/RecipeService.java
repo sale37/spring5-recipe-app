7a11a524edf63ca5f3b217810f5ecdf3f2f1c8f0
Sasa Ilic <sale-37@live.com>
2018-11-02 19:31:29 +0100

    Added command objects...

--- src/main/java/guru/springframework/spring5recipeapp/service/RecipeService.java
+++ src/main/java/guru/springframework/spring5recipeapp/service/RecipeService.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5recipeapp.service;
 
+import guru.springframework.spring5recipeapp.commands.RecipeCommand;
 import guru.springframework.spring5recipeapp.model.Recipe;
 
 import java.util.Set;
@@ -7,4 +8,8 @@ import java.util.Set;
 public interface RecipeService {
 
     Set<Recipe> getRecipes();
+
+    Recipe findById(Long id);
+
+    RecipeCommand saveRecipeCommand(RecipeCommand recipeCommand);
 }
--- src/test/java/guru/springframework/spring5recipeapp/service/RecipeSeviceImplTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/service/RecipeSeviceImplTest.java
@@ -1,5 +1,7 @@
 package guru.springframework.spring5recipeapp.service;
 
+import guru.springframework.spring5recipeapp.converters.RecipeCommandToRecipe;
+import guru.springframework.spring5recipeapp.converters.RecipeToRecipeCommand;
 import guru.springframework.spring5recipeapp.model.Recipe;
 import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
 import org.junit.Before;
@@ -8,38 +10,60 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.*;
 
 public class RecipeSeviceImplTest {
 
-    RecipeSeviceImpl recipeSevice;
+    RecipeSeviceImpl recipeService;
 
     @Mock
     RecipeRepository recipeRepository;
 
+    @Mock
+    RecipeToRecipeCommand recipeToRecipeCommand;
+
+    @Mock
+    RecipeCommandToRecipe recipeCommandToRecipe;
+
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        recipeSevice = new RecipeSeviceImpl(recipeRepository);
+        recipeService = new RecipeSeviceImpl(recipeRepository, recipeCommandToRecipe, recipeToRecipeCommand);
     }
 
     @Test
-    public void getRecipes() {
+    public void getRecipeByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull("Null recipe returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
+    public void getRecipesTest() {
 
         Recipe recipe = new Recipe();
         HashSet recipeData = new HashSet();
         recipeData.add(recipe);
 
-        when(recipeSevice.getRecipes()).thenReturn(recipeData);
+        when(recipeService.getRecipes()).thenReturn(recipeData);
 
-        Set<Recipe> recipes = recipeSevice.getRecipes();
+        Set<Recipe> recipes = recipeService.getRecipes();
 
         assertEquals(recipes.size(), 1);
         verify(recipeRepository, times(1)).findAll();
+        verify(recipeRepository, never()).findById(anyLong());
     }
 }
